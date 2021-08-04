280abab63b7a8c1fd8b8e794ad51606f6ba4e617
Andrii Lypovetskyi <li_andr@ukr.net>
2021-08-04 23:13:34 +0300

    Task

--- js/script-01.js
+++ js/script-01.js
@@ -4,7 +4,11 @@
 // то кол - во миллисекунд которое передали во время вызова функции delay.
 
 const delay = ms => {
-    // Твой код
+  return  new Promise((resolve) => {
+        setTimeout(() => {
+           resolve(ms);
+        }, ms);
+    });
 };
 
 const logger = time => console.log(`Resolved after ${time}ms`);
